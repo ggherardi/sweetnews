df901566774c11cf45c59c1b6a37775cb13c3f4f
Gianmattia Gherardi <giamalfred@yahoo.it>
2018-10-10 16:09:57 +0200

    Commit

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -65,7 +65,7 @@ class Menu {
             var arePermissionsValid = item.needPermissions ? sharedStorage.loginContext.delega_codice >= item.needPermissions : true;
             if(arePermissionsValid && item.showInMenu == undefined || item.showInMenu) {          
                 this.html += `<li id='navbar__${item.name}'>
-                                <a onclick='Menu.menuClick(this);' data-view='${item.name}'>${item.title}</a>
+                                </span><a onclick='Menu.menuClick(this);' data-view='${item.name}'>${item.title}</a>
                             </li>`;   
             }                
         }
