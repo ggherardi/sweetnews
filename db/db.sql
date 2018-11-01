ff7e11ab7cebd34d0a755c5edb6527614d97902b
Gianmattia <giamalfred@yahoo.it>
2018-11-01 20:09:54 +0100

    Commit

--- db/db.sql
+++ db/db.sql
@@ -441,8 +441,8 @@ CREATE TABLE `ricetta_salvata` (
   `id_ricetta` int(11) NOT NULL,
   PRIMARY KEY (`id_utente`,`id_ricetta`),
   KEY `fk_lista_ricerche_ricetta_idx` (`id_ricetta`),
-  CONSTRAINT `fk_lista_ricerche_ricetta` FOREIGN KEY (`id_ricetta`) REFERENCES `ricetta` (`id_ricetta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_lista_ricerche_utente` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_ricetta_salvata_ricetta` FOREIGN KEY (`id_ricetta`) REFERENCES `ricetta` (`id_ricetta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ricetta_salvata_utente` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -812,4 +812,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-01 19:34:44
+-- Dump completed on 2018-11-01 20:09:42
